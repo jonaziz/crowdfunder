a21ca60fab8d55225213172c52c90b652443d5c3
Jon Aziz <jonaziz@gmail.com>
2013-04-09 10:36:42 -0400

    Add testing setup

---
+++ test/factories/projects.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :project do
+  	user
+  	title: "WiFi-sniffing Shoes"
+  	teaser: "Find nearby networks just by walking!"
+  	description: "These are really great shoes."
+  	goal: 100000
+  end
+end
